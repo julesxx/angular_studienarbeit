f02338dc85e23c6e124be7d07af0514f22eea837
Joel WInter <jw096@hdm-stuttgart.de>
2017-08-22 16:14:59 +0200

    Anpassungen an der Hauptkomponente

--- angular-studienarbeit/src/app/app.module.ts
+++ angular-studienarbeit/src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Import HttpClientModule from @angular/common/http
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { DatatableComponent } from './datatable/datatable.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import {LoginService} from "./login.service";
+
+
+
+const appRoutes: Routes = [
+  { path: 'todoList', component: DatatableComponent },
+  { path: 'contact',      component:LoginComponentComponent},
+  { path: '',
+    redirectTo: '/contact',
+    pathMatch: 'full'
+  }
+  ]
 @NgModule({
   declarations: [
     AppComponent,
     DatatableComponent,
-    HomeComponentComponent
+    HomeComponentComponent,
+    LoginComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
